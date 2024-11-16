4462c275271cf5f392010cdef66425e26016e078
Tommy Heng <tommy.seng.heng@gmail.com>
2024-11-16 10:37:54 -0500

    feat: add Progressive Web App configurations for immersive on mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/theng-web-apps",
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        "name": "Theng Web Apps",
+        "short_name": "Theng Apps",
+        "start_url": "/index.html",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#000000",
+        "icons": [
+          {
+            "src": "icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              ['script', 'style', 'document'].includes(request.destination),
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources-cache',
+            },
+          },
+        ],
+      },
+    }),
+    react()],
 })
