2df2a40c273543695a636ff07d98f03718e44102
Tommy Heng <tommy.seng.heng@gmail.com>
2024-11-17 15:13:13 -0500

    feat: new color theme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, IconButton, Toolbar } from '@mui/material';
+import { AppBar, IconButton, Toolbar, useTheme } from '@mui/material';
 import React from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import { useNavigate } from 'react-router-dom';
@@ -7,12 +7,13 @@ type NavBarProps = {};
 
 export const NavBar: React.FC<NavBarProps> = () => {
   const navigate = useNavigate();
+  const theme = useTheme();
   return (
     <>
       <AppBar>
         <Toolbar variant="dense">
           <IconButton onClick={() => navigate('')}>
-            <HomeIcon />
+            <HomeIcon sx={{ color: theme.palette.primary.contrastText }} />
           </IconButton>
         </Toolbar>
       </AppBar>
--- src/routes/MainLayout.tsx
+++ src/routes/MainLayout.tsx
@@ -1,20 +1,25 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { NavBar } from '../components/NavBar';
-import { Box, CssBaseline, Stack } from '@mui/material';
+import { Box, createTheme, CssBaseline, Stack, ThemeProvider } from '@mui/material';
+import { themeOptions } from './theme';
+
+const theme = createTheme(themeOptions);
 
 export const MainLayout: React.FC = () => {
   return (
     <>
       <CssBaseline />
-      <Stack height="100vh" width="100vw" alignItems="center">
-        <NavBar />
-        <Box flex={1} overflow="auto" width="100%" maxWidth={1400} pt={2} px={2}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Outlet />
-          </Suspense>
-        </Box>
-      </Stack>
+      <ThemeProvider theme={theme}>
+        <Stack height="100vh" width="100vw" alignItems="center">
+          <NavBar />
+          <Box flex={1} overflow="auto" width="100%" maxWidth={1400} pt={2} px={2}>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Outlet />
+            </Suspense>
+          </Box>
+        </Stack>
+      </ThemeProvider>
     </>
   );
 };
