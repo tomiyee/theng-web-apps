58ebb2dae01e453296b1a64ca5953c5f9bb4c407
Tommy Heng <tommy.seng.heng@gmail.com>
2025-03-08 18:03:09 -0500

    temp

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -9,6 +9,7 @@ const FareSharePage = lazy(() => import('../views/FareShare/FareShare'));
 const NumPadTrainerPage = lazy(() => import('../views/NumPadTrainer/NumPadTrainer'));
 const RicePurityPage = lazy(() => import('../views/RicePurity/RicePurity'));
 const FoxGamePage = lazy(() => import('../views/FoxGame/FoxGame'));
+const RecursiveTicTacToePage = lazy(() => import('../views/RecursiveTicTacToe/RecursiveTicTacToe'));
 
 export const router = createBrowserRouter([
   {
@@ -35,6 +36,10 @@ export const router = createBrowserRouter([
       {
         path: 'fox-game',
         element: <FoxGamePage />,
+      },
+      {
+        path: 'recursive-tic-tac-toe',
+        element: <RecursiveTicTacToePage />,
       },
     ],
   },
---
+++ src/views/RecursiveTicTacToe/RecursiveTicTacToe.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const RecursiveTicTacToe: React.FC = () => {
+  return null;
+};
+export default RecursiveTicTacToe;
